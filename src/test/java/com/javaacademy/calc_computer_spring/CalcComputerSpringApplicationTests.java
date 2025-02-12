b6cb1b38f97db358cacd97a8fa9c4951c5e012e6
Александр <alexandrrychko@gmail.com>
2025-02-12 09:30:01 +0500

    it'ss calc_computer_spring

---
+++ src/test/java/com/javaacademy/calc_computer_spring/CalcComputerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaacademy.calc_computer_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcComputerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
